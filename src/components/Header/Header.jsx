24c3f0ab2451fdae8229a5dc24ada284b81b69b0
Trevor Lohrbeer <trevor@fastfedora.com>
2020-07-12 10:47:52 -0400

    debug: Add back in `RegionSearch`

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,13 @@
 import Head from 'next/head'
-// import RegionSearch from '../RegionSearch';
+import RegionSearch from '../RegionSearch';
 import styles from './Header.module.scss';
 
-      // <RegionSearch />
 
 export default function Header({ title }) {
   return (
     <header className={styles.header}>
       <h1>{title}</h1>
+      <RegionSearch />
     </header>
   );
 }
